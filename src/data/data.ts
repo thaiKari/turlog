61e8f6e7de633ef3c6c4b878b2756fd66581cb23
Kari Meling Johannessen <karjoh@norkart.no>
2021-07-03 12:11:32 +0200

    add date

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,15 +1,18 @@
 import { Trip } from "./types";
 
-
-export async function getTrips() : Promise<Trip[]> {
-    const trips = await( await fetch(`/api/trips`)).json();
-    return trips;    
+export async function getTrips(): Promise<Trip[]> {
+  let trips = await (await fetch(`/api/trips`)).json();
+  sanitize(trips);
+  return trips;
 }
 
-export async function getImageBaseUrl() : Promise<string> {
-    const settings = await( await fetch(`/api/settings`)).json();
-    return settings["imagesurl"]
+export async function getImageBaseUrl(): Promise<string> {
+  const settings = await (await fetch(`/api/settings`)).json();
+  return settings["imagesurl"];
 }
 
-
-
+function sanitize(trips: Trip[]) {
+  trips.forEach((t) => {
+    t.date = new Date(t.date.toString());
+  });
+}
