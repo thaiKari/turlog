f61b552dd8015fced3bc429a7b9a670b35d6740f
Kari Meling Johannessen <karjoh@norkart.no>
2021-07-03 13:00:21 +0200

    addTrip button

---
+++ src/components/commonStyles.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useCommonStyles = makeStyles((theme) =>({
+    centeredParent: {
+      display:'flex',
+      justifyContent:'center'
+    },
+    flexEndParent: {
+        display:'flex',
+        justifyContent:'flex-end',
+        marginRight: theme.spacing(6)
+      }
+  }));
