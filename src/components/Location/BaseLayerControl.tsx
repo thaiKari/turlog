b78ccfab7ae84a26ae65525c385a97d2a75480c5
Kari Meling Johannessen <karjoh@norkart.no>
2021-07-17 21:25:55 +0200

    show location on map

---
+++ src/components/Location/BaseLayerControl.tsx
@@ -0,0 +1,70 @@
+import { LatLngTuple } from 'leaflet';
+import React, { useEffect } from 'react'
+import { LayersControl, TileLayer, useMap } from 'react-leaflet'
+
+const apikey = '18169747-cdbe-4281-bf1f-6ad408255fd1';
+
+const layers = [
+    {
+        Name: 'grey',
+        Tileset: 'webatlas-gray-vektor',
+        Ext: 'png'
+    },
+    {
+        Name: 'medium',
+        Tileset: 'webatlas-medium-vektor',
+        Ext: 'png'
+    },
+    {
+        Name: 'lite',
+        Tileset: 'webatlas-lite-vektor',
+        Ext: 'png'
+    },
+    {
+        Name: 'vector',
+        Tileset: 'webatlas-standard-vektor',
+        Ext: 'png'
+    },
+    {
+        Name: 'aerial',
+        Tileset: 'webatlas-orto-newup',
+        Ext: 'jpeg'
+    },
+    {
+        Name: 'hybrid',
+        Tileset: 'webatlas-standard-hybrid',
+        Ext: 'jpeg'
+    }
+]
+
+interface Props {
+    center: LatLngTuple
+}
+
+export const BaseLayerControl = ({center}: Props) => {
+    const map = useMap()
+    const defaultLayer = 'vector';
+    
+    useEffect(() => {
+        center && map.flyTo(center)
+        map.invalidateSize();
+    }, [center, map])
+
+    useEffect(() => {
+        setTimeout(() => {
+            map.invalidateSize();
+        }, 300)
+    }, [map])
+
+    return (
+        <LayersControl position="bottomleft">
+                {layers.map(l => (
+                    <LayersControl.BaseLayer key={l.Name} checked={l.Name === defaultLayer} name={l.Name}>
+                        <TileLayer
+                            url={`https://waapi.webatlas.no/maptiles/tiles/${l.Tileset}/wa_grid/{z}/{x}/{y}.${l.Ext}?APITOKEN=${apikey}`}
+                            attribution={`Norkart.`}
+                        />
+                    </LayersControl.BaseLayer>))}
+            </LayersControl>
+    )
+}
