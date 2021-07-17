5974481736c0c0654f97f06075305663a78ce825
Kari Meling Johannessen <karjoh@norkart.no>
2021-07-17 19:09:25 +0200

    add files

---
+++ api/Models/Coordinates.cs
@@ -0,0 +1,8 @@
+﻿namespace api.Models
+{
+    public class Coordinates
+    {
+        public float Lat { get; set; }
+        public float Lng { get; set; }
+    }
+}
---
+++ api/Models/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace api.Models
+{
+    public class Location
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Kommune { get; set; }
+        public string Fylke { get; set; }
+        public Coordinates Coordinates { get; set; }
+    }
+}
