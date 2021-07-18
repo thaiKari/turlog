77e538e967af1562ec7e7473658d63ce053b0f0e
Kari Meling Johannessen <karjoh@norkart.no>
2021-07-18 17:00:32 +0200

    use a tag to trigger auth events

--- src/components/Login/LoginButtons.tsx
+++ src/components/Login/LoginButtons.tsx
@@ -3,7 +3,6 @@ import MicrosoftButton from './icons/signInMicrosoft.png'
 import GoogleButton from './icons/signInGoogle.png'
 import FacebookButton from './icons/signInFacebook.png'
 import React from 'react'
-import { useHistory } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
     buttonImage: {
@@ -14,26 +13,24 @@ const useStyles = makeStyles((theme) => ({
 
 export const LoginButtons = () => {
     const classes = useStyles();
-    const history = useHistory();
-
 
     return (
         <>
-            <Button
-                onClick={() => history.push('/.auth/login/facebook')}
-            >
-                <img className={classes.buttonImage} alt='Sign in with Facebook' src={FacebookButton} />
+            <Button>
+                <a href='/.auth/login/facebook'>
+                    <img className={classes.buttonImage} alt='Sign in with Facebook' src={FacebookButton} />
+                </a>
             </Button>
 
-            <Button
-                onClick={() => history.push('/.auth/login/google')}
-            >
-                <img className={classes.buttonImage} alt='Sign in with Google' src={GoogleButton} />
+            <Button>
+                <a href='/.auth/login/google'>
+                    <img className={classes.buttonImage} alt='Sign in with Google' src={GoogleButton} />
+                </a>
             </Button>
-            <Button
-                onClick={() => history.push('/.auth/login/aad')}
-            >
-                <img className={classes.buttonImage} alt='Sign in with Microsoft' src={MicrosoftButton} />
+            <Button>
+                <a href='/.auth/login/aad'>
+                    <img className={classes.buttonImage} alt='Sign in with Microsoft' src={MicrosoftButton} />
+                </a>
             </Button>
         </>
     )
--- src/components/Login/LoginPage.tsx
+++ src/components/Login/LoginPage.tsx
@@ -1,16 +1,18 @@
 import { Button, makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
-import { useHistory } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { isLoggedInState } from '../../data/user';
 import { LoginButtons } from './LoginButtons';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        width:'100%',
-        display:'flex',
-        flexDirection:'column',
-        alignItems:'center',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        '& a':{
+            textDecoration: 'none'
+        }
     },
     title: {
         margin: theme.spacing(1),
@@ -21,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
 export const LoginPage = () => {
     const classes = useStyles();
     const isLoggedIn = useRecoilValue(isLoggedInState);
-    const history = useHistory();
+
 
     if (isLoggedIn) {
         return (
@@ -29,12 +31,11 @@ export const LoginPage = () => {
                 <Typography paragraph>
                     You are already logged in.
                 </Typography>
-                <Button 
-                variant='contained' color='primary'
-                onClick={() => history.push('/logout?post_logout_redirect_uri=/')}
-                >
-                    Log out
-                </Button>
+                <a href='/.auth/logout?post_logout_redirect_uri=/'>
+                    <Button variant='contained' color='primary'>
+                        Log out
+                    </Button>
+                </a>
             </div>
         )
     }
@@ -44,7 +45,7 @@ export const LoginPage = () => {
             <Typography className={classes.title} variant="h5" component="h2">
                 Log in
             </Typography>
-            <LoginButtons/>            
+            <LoginButtons />
         </div>
     )
 }
