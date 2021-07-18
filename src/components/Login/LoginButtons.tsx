0acf4ba84d4860994154905742e8d7154adcf473
Kari Meling Johannessen <karjoh@norkart.no>
2021-07-18 16:36:23 +0200

    go to login on button clicks

--- src/components/Login/LoginButtons.tsx
+++ src/components/Login/LoginButtons.tsx
@@ -3,6 +3,7 @@ import MicrosoftButton from './icons/signInMicrosoft.png'
 import GoogleButton from './icons/signInGoogle.png'
 import FacebookButton from './icons/signInFacebook.png'
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
     buttonImage: {
@@ -13,17 +14,26 @@ const useStyles = makeStyles((theme) => ({
 
 export const LoginButtons = () => {
     const classes = useStyles();
+    const history = useHistory();
+
 
     return (
         <>
-            <Button>
+            <Button
+                onClick={() => history.push('/.auth/login/facebook')}
+            >
                 <img className={classes.buttonImage} alt='Sign in with Facebook' src={FacebookButton} />
             </Button>
-            <Button>
-                <img className={classes.buttonImage} alt='Sign in with Microsoft' src={MicrosoftButton} />
-            </Button>
-            <Button>
+
+            <Button
+                onClick={() => history.push('/.auth/login/google')}
+            >
                 <img className={classes.buttonImage} alt='Sign in with Google' src={GoogleButton} />
+            </Button>
+            <Button
+                onClick={() => history.push('/.auth/login/aad')}
+            >
+                <img className={classes.buttonImage} alt='Sign in with Microsoft' src={MicrosoftButton} />
             </Button>
         </>
     )
--- src/components/Login/LoginPage.tsx
+++ src/components/Login/LoginPage.tsx
@@ -1,5 +1,8 @@
-import { makeStyles, Typography } from '@material-ui/core'
+import { Button, makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
+import { useHistory } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { isLoggedInState } from '../../data/user';
 import { LoginButtons } from './LoginButtons';
 
 const useStyles = makeStyles((theme) => ({
@@ -17,6 +20,24 @@ const useStyles = makeStyles((theme) => ({
 
 export const LoginPage = () => {
     const classes = useStyles();
+    const isLoggedIn = useRecoilValue(isLoggedInState);
+    const history = useHistory();
+
+    if (isLoggedIn) {
+        return (
+            <div className={classes.root}>
+                <Typography paragraph>
+                    You are already logged in.
+                </Typography>
+                <Button 
+                variant='contained' color='primary'
+                onClick={() => history.push('/logout?post_logout_redirect_uri=/')}
+                >
+                    Log out
+                </Button>
+            </div>
+        )
+    }
 
     return (
         <div className={classes.root}>
